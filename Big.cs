2dc27dd1d14143ea0bc18d58eba1dcef8edd8163
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2022-04-26 14:25:48 +0200

    Update Big.cs
    sub and add

--- Big.cs
+++ Big.cs
@@ -34,4 +34,146 @@ public class BigInteger
             _numbers[i] = value[i];
         }
     }
+    private BigInteger Sub(BigInteger another)
+    {
+        if (_isPositive && !another._isPositive)
+            return Add(new BigInteger(another.ToString().Remove(0, 1)));
+        if (!_isPositive && another._isPositive)
+            return new BigInteger("-" + another.Add(new BigInteger(ToString().Remove(0, 1))));
+        if (!_isPositive && !another._isPositive)
+        {
+            BigInteger modAnother = new BigInteger(another.ToString().Remove(0, 1));
+            BigInteger modFirst = new BigInteger(this.ToString().Remove(0, 1));
+            return modAnother.Sub(modFirst);
+        }
+
+        string result = "";
+        string firstNum;
+        string secondNum;
+        bool negative = false;
+        if (FindBigger(ToString(), another.ToString()) == ToString())
+        {
+            firstNum = ToString();
+            secondNum = another.ToString();
+        }
+        else
+        {
+            negative = true;
+            firstNum = another.ToString();
+            secondNum = ToString();
+        }
+        if (firstNum.Length > secondNum.Length)
+        {
+            int difference = firstNum.Length - secondNum.Length;
+            for (int i = 0; i < difference; i++)
+            {
+                secondNum = "0" + secondNum;
+            }
+        }
+        bool toTake = false;
+        for (int i = secondNum.Length - 1; i >= 0; i--)
+        {
+            int takeIndex = i;
+            while (toTake)
+            {
+                if (firstNum[takeIndex] - '0' >= 1)
+                {
+                    int newDigit = firstNum[takeIndex] - '0' - 1;
+                    firstNum = replaceByIndex(firstNum, takeIndex, Convert.ToChar(newDigit.ToString()));
+                    toTake = false;
+                }
+                else
+                {
+                    firstNum = replaceByIndex(firstNum, takeIndex, '9');
+                    takeIndex--;
+                }
+            }
+            int diff;
+            if (firstNum[i] - '0' >= secondNum[i] - '0')
+            {
+                diff = (firstNum[i] - '0') - (secondNum[i] - '0');
+                result = diff + result;
+            }
+            else
+            {
+                toTake = true;
+                string modifiedFirst = "1" + firstNum[i];
+                diff = Int32.Parse(modifiedFirst) - (secondNum[i] - '0');
+                result = diff + result;
+            }
+        }
+
+        while (result[0] == '0')
+        {
+            result = result.Remove(0, 1);
+        }
+        return negative ? new BigInteger("-" + result) : new BigInteger(result);
+    }
+    private BigInteger Add(BigInteger another)
+    {
+        if (_isPositive && !another._isPositive)
+            return Sub(new BigInteger(another.ToString().Remove(0, 1)));
+        if (!_isPositive && another._isPositive)
+            return another.Sub(new BigInteger(this.ToString().Remove(0, 1)));
+        if (!_isPositive && !another._isPositive)
+        {
+            BigInteger modAnother = new BigInteger(another.ToString().Remove(0, 1));
+            BigInteger modFirst = new BigInteger(this.ToString().Remove(0, 1));
+            return new BigInteger("-" + modFirst.Add(modAnother));
+        }
+        string result = "";
+        string firstNum = ToString();
+        string secNum = another.ToString();
+        if (firstNum.Length > secNum.Length)
+        {
+            int difference = firstNum.Length - secNum.Length;
+            for (int i = 0; i < difference; i++)
+            {
+                secNum = "0" + secNum;
+            }
+        }
+        else if (secNum.Length > firstNum.Length)
+        {
+            int difference = secNum.Length - firstNum.Length;
+            for (int i = 0; i < difference; i++)
+            {
+                firstNum = "0" + firstNum;
+            }
+        }
+        string toCount = "";
+        for (int i = secNum.Length - 1; i >= 0; i--)
+        {
+            if (toCount != "")
+            {
+                for (int y = i; y >= -1; y--)
+                {
+                    if (y == -1)
+                    {
+                        firstNum = '1' + firstNum;
+                        secNum = '0' + secNum;
+                        i += 1;
+                        break;
+                    }
+                    if (firstNum[y] != '9')
+                    {
+                        int digit = firstNum[y] - '0' + 1;
+                        firstNum = replaceByIndex(firstNum, y, Convert.ToChar(digit.ToString()));
+                        break;
+                    }
+                    
+                    {
+                        firstNum = replaceByIndex(firstNum, y, '0');
+                    }
+                }
+            }
+            toCount = Convert.ToString(firstNum[i] - '0' + secNum[i] - '0');
+            result = toCount[^1] + result;
+            toCount = toCount.Remove(0, 1);
+        }
+        if (toCount != "")
+        {
+            result = '1' + result;
+        }
+        return new BigInteger(result);
+    }
 }
