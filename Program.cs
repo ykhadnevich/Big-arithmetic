1f2c700e028ca6cad88ba1b0979bde9ccbcb3525
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2022-04-26 14:38:10 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using ConsoleApp4;
+
+BigInteger first1 = new BigInteger("666600");
+BigInteger second1 = new BigInteger("54333");
+Console.WriteLine(first1 + second1);
+Console.WriteLine(first1 - second1);
+
+Console.Write(Karatsuba.Multiply("-3000000000000000", "2000000000000000"));
+Console.Read();
